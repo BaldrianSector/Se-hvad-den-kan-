84680de46505555f7441404fe60fadc0569cdd56
Baldrian Sector <baldriansector@gmail.com>
2023-09-07 00:07:18 +0200

    li from data and made clickable with eventlistener

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ let priceCatalog = {}
 // Adds objects into priceCatalog as objects with key, itemName, price and a source if avaliable 
 function populatePriceCatalog(itemName, price, source = "missing", emoji = undefined, gender = undefined) {
     
-    // Define key to be itemName with all lowercase and replace whitespace with "-"
-    const key = itemName.replace(/\s+/g, '-').toLowerCase()
- 
+    const key = convertNameToKey(itemName)
+    
     // Create an object with the desired structure
     const itemData = {
-      "key": key,
-      "name": itemName,
-      "price": price,
-      "source": source,
-      "emoji": emoji,
-      "gender": gender
+        "key": key,
+        "name": itemName,
+        "price": price,
+        "source": source,
+        "emoji": emoji,
+        "gender": gender
     };
-  
+    
     // Add the item data to the priceCatalog using the provided key
     priceCatalog[key] = itemData;
+}
+
+// Define key to be itemName with all lowercase and replace whitespace with "-"
+
+function convertNameToKey(itemName) {
+    return itemName.replace(/\s+/g, '-').toLowerCase()
 }
 
 // List of items **** Put this into JSON file ****
@@ -52,10 +57,12 @@ function readValue(itemKey, parameter) {
     return priceCatalog[itemKey][parameter];
 }
 
-// Returns value of price
+// Returns value of price by itemKey
 function readPrice(itemKey) {
     return priceCatalog[itemKey].price
 }
+
+
 
 // Formats numbers to 2 decimals **** Needs edgecase protection against values less than 0.00 ****
 function formatNumber(number) {
@@ -79,8 +86,8 @@ function article(itemKey) {
 
 // Compares and devides 2 items
 function compareItems(item1,item2) {
-    const value1 = readPrice(item1)
-    const value2 = readPrice(item2)
+    const value1 = readValue(item1, "price")
+    const value2 = readValue(item2, "price")
 
     const itemName1 = readValue(item1,"name")
     const itemName2 = readValue(item2,"name")
@@ -88,7 +95,91 @@ function compareItems(item1,item2) {
     console.log(`${article(item1)} ${itemName1} svarer til ${formatNumber(value1/value2)} ${itemName2}`);
 }
 
-compareItems("liter-letmælk", "tesla-model-3")
-
 // Output: En item1 svarter til x item2. Det vil sige at en 1 koster en 1/x'ne del af en item2. En item1 koster x gange så meget som en item2.
 // Game, equalize the money on the weight left side = right side
+
+// Dropdown / Searchbox
+
+// Function to populate both lists with items from priceCatalog
+function populateLists() {
+    const listContainer = document.querySelector(".list-container");
+
+    // Create ul elements for both lists
+    const ul1 = document.createElement("ul");
+    const ul2 = document.createElement("ul");
+
+    // Loop through priceCatalog and populate both lists
+    Object.keys(priceCatalog).forEach(key => {
+        let objectInsideObject = priceCatalog[key];
+
+        // Create li elements for each item in both lists
+        const li1 = document.createElement("li");
+        const li2 = document.createElement("li");
+
+        // Set the li's text content to the item's name
+        li1.textContent = objectInsideObject.name;
+        li2.textContent = objectInsideObject.name;
+
+        // Append the li elements to the respective ul elements
+        ul1.appendChild(li1);
+        ul2.appendChild(li2);
+    });
+
+    // Append both ul elements to the list container
+    document.getElementById("list1").appendChild(ul1);
+    document.getElementById("list2").appendChild(ul2);
+}
+
+populateLists();
+
+
+// Get all list items in list1 and list2
+const list1Items = document.querySelectorAll("#list1 li");
+const list2Items = document.querySelectorAll("#list2 li");
+
+let selectedItems = []
+
+// Add click event listeners to list1 items
+list1Items.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Remove the "selected" class from all list1 items
+    list1Items.forEach((li) => {
+      li.classList.remove("selected");
+    });
+
+    // Add the "selected" class to the clicked list1 item
+    item.classList.add("selected");
+
+    // Log the value of the clicked item
+    const clickedItemValue = item.textContent;
+    selectedItems[0] = clickedItemValue
+    console.log(selectedItems[0])
+    console.log(`Clicked item value: ${clickedItemValue}`);
+    getSelectedItemCount() === 2 ? console.log(compareItems(convertNameToKey(selectedItems[0],convertNameToKey(selectedItems[1])))) : console.log("Items not selected yet.")
+  });
+});
+
+// Add click event listeners to list2 items
+list2Items.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Remove the "selected" class from all list2 items
+    list2Items.forEach((li) => {
+      li.classList.remove("selected");
+    });
+
+    // Add the "selected" class to the clicked list2 item
+    item.classList.add("selected");
+
+    // Log the value of the clicked item
+    const clickedItemValue = item.textContent;
+    selectedItems[1] = clickedItemValue
+    console.log(selectedItems[1])
+    console.log(`Clicked item value: ${clickedItemValue}`);
+    getSelectedItemCount() === 2 ? console.log(compareItems(convertNameToKey(selectedItems[0],convertNameToKey(selectedItems[1])))) : console.log("Items not selected yet.")
+  });
+});
+
+function getSelectedItemCount() {
+    const selectedItems = document.querySelectorAll(".selected");
+    return selectedItems.length;
+  }

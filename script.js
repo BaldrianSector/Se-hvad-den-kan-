ddeceac289185f36fe0ba24e8547d713a1cbf403
Baldrian Sector <baldriansector@gmail.com>
2023-09-07 15:39:47 +0200

    Fixed typo in iPad Pro

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Hook for DOM
+
+const textEl = document.querySelector("#text-el")
+
 let priceCatalog = {}
 
 // Adds objects into priceCatalog as objects with key, itemName, price and a source if avaliable 
@@ -31,7 +35,7 @@ populatePriceCatalog("Liter Letmælk", 11, "https://www.nemlig.com/dagligvarer/m
 populatePriceCatalog("Citronmåne", 20, "https://www.nemlig.com/citronmaane-904031", "🍰", true)
 populatePriceCatalog("Netflix Abonnement", 114, "https://www.hvadkosterdet.dk/project/hvad-koster-netflix/", "🍿", false)
 populatePriceCatalog("Parkeringsbøde", 830, "https://www.q-park.dk/da/nyheder/afgiftssats-2022/", "🅿", true)
-populatePriceCatalog("iPad Pro", undefined, 9999, true)
+populatePriceCatalog("iPad Pro", 9999, undefined, undefined, true)
 populatePriceCatalog("iPhone 14 Pro", 10499, undefined, "📱", true)
 populatePriceCatalog("F-16", 14000000, undefined, "✈", false)
 populatePriceCatalog("Tesla Model 3", 1185000, undefined, "⚡", true)
@@ -64,14 +68,17 @@ function readPrice(itemKey) {
 
 
 
-// Formats numbers to 2 decimals **** Needs edge case protection against values less than 0.01 ****
+// Formats numbers with dots for readability and commas as separators for decimal numbers
 function formatNumber(number) {
     if (number > 0.01) {
-        const maximumDecimalPlaces = 2;
-        const formattedNumber = number.toFixed(maximumDecimalPlaces);
+        const formattedNumber = number.toLocaleString('en-US', {
+            minimumFractionDigits: 2, // Always show at least 2 decimal places
+            maximumFractionDigits: 2, // Show a maximum of 2 decimal places
+            useGrouping: true, // Use grouping separators (commas)
+        });
         return formattedNumber;
     } else {
-        return number;
+        return number.toLocaleString('en-US', { maximumFractionDigits: 20 }); // Format smaller numbers without decimals to return the full number
     }
 }
 
@@ -91,41 +98,38 @@ function compareItems(item1,item2) {
     
     const itemName1 = readValue(item1,"name")
     const itemName2 = readValue(item2,"name")
-        
+    
     console.log(`${article(item1)} ${itemName1} svarer til ${formatNumber(value1/value2)} ${itemName2}`);
-    return "compareItems has been completed"
+
+    comparisonResult = (`${article(item1)} ${itemName1} svarer til ${formatNumber(value1/value2)} ${itemName2}. Det betyder at ${article(item2).toLowerCase()} ${itemName2} svarer til ${formatNumber(value2/value1)} ${itemName1}.`)
+    return "compareItems() has been completed"
 }
-
-// Output: En item1 svarter til x item2. Det vil sige at en 1 koster en 1/x'ne del af en item2. En item1 koster x gange så meget som en item2.
-// Game, equalize the money on the weight left side = right side
-
-// Dropdown / Searchbox
 
 // Function to populate both lists with items from priceCatalog
 function populateLists() {
     const listContainer = document.querySelector(".list-container");
-
+    
     // Create ul elements for both lists
     const ul1 = document.createElement("ul");
     const ul2 = document.createElement("ul");
-
+    
     // Loop through priceCatalog and populate both lists
     Object.keys(priceCatalog).forEach(key => {
         let objectInsideObject = priceCatalog[key];
-
+        
         // Create li elements for each item in both lists
         const li1 = document.createElement("li");
         const li2 = document.createElement("li");
-
+        
         // Set the li's text content to the item's name
         li1.textContent = objectInsideObject.name;
         li2.textContent = objectInsideObject.name;
-
+        
         // Append the li elements to the respective ul elements
         ul1.appendChild(li1);
         ul2.appendChild(li2);
     });
-
+    
     // Append both ul elements to the list container
     document.getElementById("list1").appendChild(ul1);
     document.getElementById("list2").appendChild(ul2);
@@ -142,55 +146,59 @@ let selectedItems = []
 let clickedItemKey1 = ""
 let clickedItemKey2 = ""
 
+let comparisonResult = ""
 
 // Add click event listeners to list1 items
 list1Items.forEach((item) => {
-  item.addEventListener("click", () => {
-    // Remove the "selected" class from all list1 items
-    list1Items.forEach((li) => {
-      li.classList.remove("selected");
+    item.addEventListener("click", () => {
+        // Remove the "selected" class from all list1 items
+        list1Items.forEach((li) => {
+            li.classList.remove("selected");
+        });
+        
+        // Add the "selected" class to the clicked list1 item
+        item.classList.add("selected");
+        
+        // Log the value of the clicked item
+        const clickedItemValue1 = item.textContent;
+        selectedItems[0] = clickedItemValue1
+        
+        clickedItemKey1 = convertNameToKey(selectedItems[0])
+        
+        console.log(`Clicked item value: "${clickedItemValue1}" in list 1 with an itemKey of "${clickedItemKey1}"`);
+        
+        getSelectedItemCount() === 2 ? console.log(compareItems(clickedItemKey1,clickedItemKey2)) : console.log("Two items has not been selected yet.")
+        
+        textEl.innerText = comparisonResult
     });
-
-    // Add the "selected" class to the clicked list1 item
-    item.classList.add("selected");
-
-    // Log the value of the clicked item
-    const clickedItemValue1 = item.textContent;
-    selectedItems[0] = clickedItemValue1
-
-    clickedItemKey1 = convertNameToKey(selectedItems[0])
-
-    console.log(`Clicked item value: "${clickedItemValue1}" in list 1 with an itemKey of "${clickedItemKey1}"`);
-
-    getSelectedItemCount() === 2 ? console.log(compareItems(clickedItemKey1,clickedItemKey2)) : console.log("Two items has not been selected yet.")
-
-});
 });
 
 // Add click event listeners to list2 items
 list2Items.forEach((item) => {
-  item.addEventListener("click", () => {
-    // Remove the "selected" class from all list2 items
-    list2Items.forEach((li) => {
-      li.classList.remove("selected");
+    item.addEventListener("click", () => {
+        // Remove the "selected" class from all list2 items
+        list2Items.forEach((li) => {
+            li.classList.remove("selected");
+        });
+        
+        // Add the "selected" class to the clicked list2 item
+        item.classList.add("selected");
+        
+        // Log the value of the clicked item
+        const clickedItemValue2 = item.textContent;
+        selectedItems[1] = clickedItemValue2
+        
+        clickedItemKey2 = convertNameToKey(selectedItems[1])
+        
+        console.log(`Clicked item value: "${clickedItemValue2}" in list 2 with an itemKey of "${clickedItemKey2}"`);
+        
+        getSelectedItemCount() === 2 ? console.log(compareItems(clickedItemKey1,clickedItemKey2)) : console.log("Two items has not been selected yet.")
+        
+        textEl.innerText = comparisonResult
     });
-
-    // Add the "selected" class to the clicked list2 item
-    item.classList.add("selected");
-
-    // Log the value of the clicked item
-    const clickedItemValue2 = item.textContent;
-    selectedItems[1] = clickedItemValue2
-    
-    clickedItemKey2 = convertNameToKey(selectedItems[1])
-
-    console.log(`Clicked item value: "${clickedItemValue2}" in list 2 with an itemKey of "${clickedItemKey2}"`);
-    
-    getSelectedItemCount() === 2 ? console.log(compareItems(clickedItemKey1,clickedItemKey2)) : console.log("Two items has not been selected yet.")
-  });
 });
 
 function getSelectedItemCount() {
     const selectedItems = document.querySelectorAll(".selected");
     return selectedItems.length;
-  }
+}
